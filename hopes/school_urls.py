b0ca8aa5c83658eaafd847cf949b07110386a8cb
sarit <cs.sarit@gmail.com>
2016-04-05 17:38:53 +0700

    rename file

--- hopes/school_urls.py
+++ hopes/school_urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     url(r'^create/$', CreateStudent.as_view(), name='create_student'),
     url(r'^edit/(?P<pk>\d+)/$', EditStudent.as_view(), name='edit_student'),
     url(r'^delete/(?P<pk>\d+)/$', DeleteStudent.as_view(), name='delete_student'),
+    url(r'^$', ListSchools.as_view(), name='list_schools'),
+    url(r'^create/$', CreateSchool.as_view(), name='create_school'),
+    url(r'^edit/(?P<pk>\d+)/$', EditSchool.as_view(), name='edit_school'),
+    url(r'^delete/(?P<pk>\d+)/$', DeleteSchool.as_view(), name='delete_school'),
 ]
 
