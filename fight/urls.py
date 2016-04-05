b0715de437cb930b00c344b06d9a0f11b4eb05ad
sarit <cs.sarit@gmail.com>
2016-04-05 17:41:42 +0700

    split included file

--- fight/urls.py
+++ fight/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^students/', include('hopes.urls')),
+    url(r'^students/', include('hopes.student_urls')),
+    url(r'^schools/', include('hopes.school_urls')),
 ]
