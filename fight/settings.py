bf3401a504c337ba89c73db317ef15890132e27e
sarit <cs.sarit@gmail.com>
2016-04-12 18:09:20 +0700

    Change settinng.py because JSONField require postgresql database

--- fight/settings.py
+++ fight/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'fight.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "saritdb",
+        "USER": "el",
+        "PASSWORD": "Codium123!",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
